4f2337305038aa2ad11427880add9dd2e9c978cc
adi00026 <adi00026@gmail.com>
2018-08-23 11:39:42 -0400

    Figured out nested not and neg

--- codegeneration.py
+++ codegeneration.py
@@ -1,12 +1,17 @@
-from ASTNodes import Program, Function, Return, Constant
+from ASTNodes import Program, Function, Return, Constant, UnOp
+
+op_list = []
 
 '''
-Generates x86 assembly.
+Creates assembly.s
 '''
 def create(ast):
 	file = open("assembly.s", "w") 
 	parseTree(ast, file)
 
+'''
+Traverses the syntax tree and generates assembly to assembly.s
+'''
 def parseTree(ast, file):
 
 	curr = ast
@@ -22,11 +27,29 @@ def parseTree(ast, file):
 			curr = curr.getStatement()
 		
 		elif isinstance(curr, Return):
-			ret_val = curr.getExpression().getValue()
-			file.write("\tmovl $" + ret_val + ", %eax\n\tret")
-			curr = curr.getExpression()
+			if isinstance(curr.next(), Constant):
+				ret_val = curr.getExpression().getValue()
+				file.write("\tmovl $" + ret_val + ", %eax\n\tret\n")
+			
+			curr = curr.next()
 
 		elif isinstance(curr, Constant):
+			curr = curr.next()
+
+		elif isinstance(curr, UnOp):
+			if isinstance(curr.getExpression(), Constant):
+				op_list.append(curr.getOperator())
+				ret_val = curr.getExpression().getValue()
+				file.write("\tmovl $" + ret_val + ", %eax\n")
+				while len(op_list) != 0:
+					op = op_list.pop()
+					if op == "-":
+						file.write("\tneg %eax\n")
+					elif op == "~":
+						file.write("\tnot %eax\n")
+				file.write("\tret\n")
+			elif isinstance(curr.getExpression(), UnOp):
+				op_list.append(curr.getOperator())
 			curr = curr.next()
 
 	file.close()
